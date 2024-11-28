19b452676655e09a6efda6e4421e476b616c4bea
Kerlon Ribeiro <124305955+kerlonr@users.noreply.github.com>
2024-11-27 22:16:29 -0300

    Add files via upload

--- scripts/animation.js
+++ scripts/animation.js
@@ -26,6 +26,45 @@ gsap.to(".from-left-2", {
   },
 });
 
+gsap.to(".from_left_project_1", {
+  x: 0,
+  opacity: 1,
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".from_left_project_1",
+    start: "top center",
+    end: "bottom top",
+    toggleActions: "play none none reverse",
+  },
+});
+
+gsap.to(".from_left_project_2", {
+  x: 0,
+  opacity: 1,
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".from_left_project_2",
+    start: "top center",
+    end: "bottom top",
+    toggleActions: "play none none reverse",
+  },
+});
+
+gsap.to(".from_left_project_3", {
+  x: 0,
+  opacity: 1,
+  duration: 1.5,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".from_left_project_3",
+    start: "top center",
+    end: "bottom top",
+    toggleActions: "play none none reverse",
+  },
+});
+
 gsap.to(".fade-in", {
   opacity: 1,
   duration: 2,
